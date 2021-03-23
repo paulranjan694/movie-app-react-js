b3a329e1ddbb97102a8a81b4777f66e4bd2e3005
paulranjan694 <paulranjan694@gmail.com>
2021-03-23 09:54:10 +0530

    Added RootRuducer concept and then added combineReducers() method

--- src/components/App.js
+++ src/components/App.js
@@ -1,72 +1,83 @@
 import React from 'react';
-import { data } from '../data';
+import {data} from '../data';
 import MovieCard from './MovieCard';
 import Navbar from './Navbar';
-import { addMovies,setShowFourites } from '../actions';
+import {addMovies, setShowFourites} from '../actions';
 
 class App extends React.Component {
+  componentDidMount () {
+    const {store} = this.props;
 
-  componentDidMount(){
-    const { store } = this.props;
-
-    store.subscribe(()=>{
-      console.log('UPDATED');
-      this.forceUpdate();
-    })
+    store.subscribe (() => {
+      console.log ('UPDATED');
+      this.forceUpdate ();
+    });
 
     // make api call
 
     // dispatch action
-    store.dispatch(addMovies(data));
+    store.dispatch (addMovies (data));
 
-    console.log('STATE ',store.getState());
+    console.log ('STATE ', store.getState ());
   }
 
-  isMovieFavourite = (movie)=>{
-    const{ favourites } =  this.props.store.getState();
+  isMovieFavourite = movie => {
+    const {movies} = this.props.store.getState ();
 
-    const index = favourites.indexOf(movie);
-    if(index !== -1){
+    const index = movies.favourites.indexOf (movie);
+    if (index !== -1) {
       return true;
     }
     return false;
-  }
+  };
 
-  onChangeTab = (val)=>{
-    this.props.store.dispatch(setShowFourites(val));
-  }
+  onChangeTab = val => {
+    this.props.store.dispatch (setShowFourites (val));
+  };
 
-  render(){
-    const {lists,favourites, showFavourites} = this.props.store.getState(); // state - {lists:[], favourites:[]}
-    console.log('RENDER ', this.props.store.getState());
+  render () {
+    const {movies} = this.props.store.getState (); // state - {movies:{},serach:{}}
+    const {lists, favourites, showFavourites} = movies;
+    console.log ('RENDER ', this.props.store.getState ());
 
     const displayMovies = showFavourites ? favourites : lists;
 
     return (
       <div className="App">
-        <Navbar/>
+        <Navbar />
         <div className="main">
           <div className="tabs">
-            <div className={`tab ${showFavourites ? '':'active-tabs'}`} onClick={()=>this.onChangeTab(false)}>Movies</div>
-            <div className={`tab ${showFavourites ? 'active-tabs' : ''}`} onClick={()=>this.onChangeTab(true)}>Favourites</div>
+            <div
+              className={`tab ${showFavourites ? '' : 'active-tabs'}`}
+              onClick={() => this.onChangeTab (false)}
+            >
+              Movies
+            </div>
+            <div
+              className={`tab ${showFavourites ? 'active-tabs' : ''}`}
+              onClick={() => this.onChangeTab (true)}
+            >
+              Favourites
+            </div>
           </div>
-  
+
           <div className="list">
-            {displayMovies.map((movie,index)=>(
-              <MovieCard 
-                key={`movie-${index}`} 
+            {displayMovies.map ((movie, index) => (
+              <MovieCard
+                key={`movie-${index}`}
                 movie={movie}
                 dispatch={this.props.store.dispatch}
-                isFavourite={this.isMovieFavourite(movie)}
+                isFavourite={this.isMovieFavourite (movie)}
               />
             ))}
           </div>
-          {displayMovies.length === 0 ? <div className="no-movies">No Movies to display!</div>:null}
+          {displayMovies.length === 0
+            ? <div className="no-movies">No Movies to display!</div>
+            : null}
         </div>
       </div>
     );
   }
-  
 }
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,39 +1,77 @@
-import { ADD_MOVIES, ADD_FAVOURITES, REMOVE_FROM_FAVOURITES, SET_SHOW_FAVOURITES } from '../actions';
+import { combineReducers } from 'redux';
+
+import {
+  ADD_MOVIES,
+  ADD_FAVOURITES,
+  REMOVE_FROM_FAVOURITES,
+  SET_SHOW_FAVOURITES,
+} from '../actions';
+
 // initial state
-const initailMovieState={
-    lists:[],
-    favourites:[],
-    showFavourites:false,
+const initailMovieState = {
+  lists: [],
+  favourites: [],
+  showFavourites: false,
 };
 
-
-export default function movies(state = initailMovieState, action){
-    
-    switch (action.type) {
-        case ADD_MOVIES:
-            return{
-                ...state,
-                lists:action.movies
-            }
-        case ADD_FAVOURITES:
-            return {
-                ...state,
-                favourites:[action.movie, ...state.favourites]
-            }
-        case REMOVE_FROM_FAVOURITES:
-            const newFilterArrays = state.favourites.filter(
-                movie => movie.Title !== action.movie.Title
-            );
-            return {
-                ...state,
-                favourites:newFilterArrays
-            }
-        case SET_SHOW_FAVOURITES:
-            return{
-                ...state,
-                showFavourites:action.val
-            }
-        default:
-            return state;
-    }
+export function movies (state = initailMovieState, action) {
+  console.log ('MOVIES REDUCER');
+  switch (action.type) {
+    case ADD_MOVIES:
+      return {
+        ...state,
+        lists: action.movies,
+      };
+    case ADD_FAVOURITES:
+      return {
+        ...state,
+        favourites: [action.movie, ...state.favourites],
+      };
+    case REMOVE_FROM_FAVOURITES:
+      const newFilterArrays = state.favourites.filter (
+        movie => movie.Title !== action.movie.Title
+      );
+      return {
+        ...state,
+        favourites: newFilterArrays,
+      };
+    case SET_SHOW_FAVOURITES:
+      return {
+        ...state,
+        showFavourites: action.val,
+      };
+    default:
+      return state;
+  }
 }
+
+const initialSearchState = {
+  result: {},
+};
+
+export function search (state = initialSearchState, action) {
+  console.log ('SEARCH REDUCER');
+
+  return {
+    state,
+  };
+}
+
+// *****Internally combineReducers works like this only****
+
+// const initialRootReducer = {
+//   movies: initailMovieState,
+//   search: initialSearchState,
+// };
+
+// export default function rootReducer (state = initialRootReducer, action) {
+//   return {
+//     movies: movies (state.movies, action),
+//     search: search (state.search, action),
+//   };
+// }
+
+export default combineReducers({
+    movies,
+    search
+})
